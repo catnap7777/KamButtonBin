aa1822df2886f046f1460ec8d4ea995d7200ba0c
km <35552602+catnap7777@users.noreply.github.com>
2022-05-10 20:39:52 -0500

    Initial Commit

---
+++ KamButtonBin/KamButtonBinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KamButtonBinApp.swift
+//  KamButtonBin
+//
+//  Created by Karen Mathes on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct KamButtonBinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
