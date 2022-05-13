ef0c24f29ac1fb06d12438f72062eb21dcef5283
km <35552602+catnap7777@users.noreply.github.com>
2022-05-13 09:51:31 -0500

    added more to rotating button

--- KamButtonBin/ButtonTypeView/Modifiers.swift
+++ KamButtonBin/ButtonTypeView/Modifiers.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct Modifiers: View {
   @State var isDisplaying = false
   var body: some View {
-    VStack(spacing: 50) {
+    VStack(spacing: 40) {
       Text("More Modifiers")
         .font(Font.custom("ChalkboardSE-bold", size: 30))
         .foregroundColor(Color("titleColor"))
-        .padding(.bottom, 50)
+        .padding(.bottom, 15)
       Button(action: {}, label: {
         Label("More Modifiers", systemImage: "applelogo")
           .padding(.all, 20)
@@ -34,7 +34,8 @@ struct Modifiers: View {
       Button(action: { self.isDisplaying.toggle()}) {
         VStack {
           HStack {
-            Text("Displaying Rotating Button")
+            //Text("Displaying Rotating Button")
+            Text("Music Genres")
               .font(Font.custom("ChalkboardSE-bold", size: 18))
               .frame(width: 325, height: 45, alignment: .trailing)
               .padding(.trailing, 10)
@@ -54,10 +55,64 @@ struct Modifiers: View {
           )
           if isDisplaying {
             List {
-              Text("Cat")
-              Text("Dog")
-              Text("Bunny")
+              //              Label("Rock", systemImage: "dollarsign.circle.fill")
+              HStack {
+                Text("Rock")
+                  .font(Font.custom("ChalkboardSE-bold", size: 20))
+                  .foregroundColor(.blue)
+                Text("- AC/DC, Chevelle, Breaking Benjamin")
+                  .font(Font.custom("ChalkboardSE", size: 12))
+                  .foregroundColor(.gray)
+                  .padding(.top, 5)
+              }
+              HStack {
+                Text("Metal")
+                  .font(Font.custom("ChalkboardSE-bold", size: 20))
+                  .foregroundColor(.blue)
+                Text("- Metallica, Slipknot, Spiritbox")
+                  .font(Font.custom("ChalkboardSE", size: 12))
+                  .foregroundColor(.gray)
+                  .padding(.top, 5)
+              }
+              HStack {
+                Text("Pop")
+                  .font(Font.custom("ChalkboardSE-bold", size: 20))
+                  .foregroundColor(.blue)
+                Text("- Taylor Swift, Doja Cat, Ed Sheeran")
+                  .font(Font.custom("ChalkboardSE", size: 12))
+                  .foregroundColor(.gray)
+                  .padding(.top, 5)
+              }
+              HStack {
+                Text("Classical")
+                  .font(Font.custom("ChalkboardSE-bold", size: 20))
+                  .foregroundColor(.blue)
+                Text("- Mozart, Bach, Beethoven")
+                  .font(Font.custom("ChalkboardSE", size: 12))
+                  .foregroundColor(.gray)
+                  .padding(.top, 5)
+              }
+              HStack {
+                Text("Blues")
+                  .font(Font.custom("ChalkboardSE-bold", size: 20))
+                  .foregroundColor(.blue)
+                //Text("- Stevie Ray Vaughan, Etta James, Ray Charles")
+                Text("- Stevie Ray Vaughan, Etta James")
+                  .font(Font.custom("ChalkboardSE", size: 12))
+                  .foregroundColor(.gray)
+                  .padding(.top, 5)
+              }
+              HStack {
+                Text("Raggae")
+                  .font(Font.custom("ChalkboardSE-bold", size: 20))
+                  .foregroundColor(.blue)
+                Text("- Bob Marley, Ziggy Marley, Jimmy Cliff")
+                  .font(Font.custom("ChalkboardSE", size: 12))
+                  .foregroundColor(.gray)
+                  .padding(.top, 5)
+              }
             }
+            .font(Font.custom("ChalkboardSE-bold", size: 12))
             .layoutPriority(isDisplaying ? 0.5 : 1.0)
             .animation(.spring())
             Spacer()
